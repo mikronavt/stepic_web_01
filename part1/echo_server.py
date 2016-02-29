b1d9076f61aec504ecfefe430c3f271487ccee61
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-02-29 16:50:00 +0300

    Bug with finding "close" string fixed.

--- part1/echo_server.py
+++ part1/echo_server.py
@@ -2,14 +2,15 @@ __author__ = 'User'
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2222))
+s.bind(('127.0.0.1', 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
     while True:
         data = conn.recv(1024)
         if not data: break
-        if str(data).lower().find("close") > 0:
+        print data.lower().find("close")
+        if data.lower().find("close") >= 0:
             break
         conn.send(data)
     conn.close()
