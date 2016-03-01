3b1cc1458d80bd1dd38f0630db7126757bc94f05
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-01 17:10:39 +0300

    Created simple server using threads and which can communicate with many clients.

--- part1/echo_server.py
+++ part1/echo_server.py
@@ -1,8 +1,12 @@
+#simple server with one connection which sends response equals to request
 __author__ = 'User'
 import socket
 
+server_host = '0.0.0.0'
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2222))
+s.bind((server_host, 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
---
+++ part1/server_threads.py
@@ -0,0 +1,25 @@
+#simple multithread server which sends response equals to request
+__author__ = 'User'
+import socket, threading
+
+
+server_host = '0.0.0.0'
+
+
+def connection(conn, addr):
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        if data.lower().find("close") >= 0:
+            break
+        conn.send(data)
+    conn.close()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((server_host, 2222))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
+    server_thread = threading.Thread(target=connection, args=(conn, addr))
+    server_thread.start()
