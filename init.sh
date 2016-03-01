d5720cd71584bb069f4d6dc761da9e8e052e8b80
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-01 17:42:55 +0300

    shell script to init + folders

--- init.sh
+++ init.sh
@@ -1,4 +1,16 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install nginx
+cd /home/box/web
+mkdir public
+mkdir uploads
+cd public
+mkdir css
+mkdir img
+mkdir js
+cd ..
+cd ..
+#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
