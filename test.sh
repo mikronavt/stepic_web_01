7ceb5d77eb3395eeac9afe9d57735d4a9d63365a
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-02 13:09:12 +0300

    update nginx.conf

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "http://localhost/uploads/123"
 curl -I http://localhost/uploads/123
+echo "http://10.42.19.225/js/test.js"
 curl -I http://10.42.19.225/js/test.js
+echo "http://10.42.19.225/uploads/test.js"
 curl -I http://10.42.19.225/uploads/test.js
