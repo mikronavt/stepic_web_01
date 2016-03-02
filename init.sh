1ecaa90d5653860e24e01a4a16366cc483c2c3a3
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-02 12:09:14 +0300

    nginx.conf + update init script

--- init.sh
+++ init.sh
@@ -10,6 +10,7 @@ mkdir img
 mkdir js
 cd /
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
