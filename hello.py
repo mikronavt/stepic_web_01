9a67d383bdfa68382ff01cce35b5c68d798dd9fd
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-03 09:48:53 +0300

    created application which generates text page with parameters for get request

---
+++ hello.py
@@ -0,0 +1,20 @@
+__author__ = 'User'
+import wsgiref.handlers
+
+
+def app(environ, start_response):
+# бизнес-логика
+    status = '200 OK'
+    headers = [
+    ('Content-Type', 'text/plain')
+    ]
+
+    body = ''
+    if environ['REQUEST_METHOD'] == 'GET' and environ['QUERY_STRING']:
+        query = environ['QUERY_STRING'].split('&');
+        for param in query:
+            body += param + '\n'
+
+    
+    start_response(status, headers)
+    return [ body ]
