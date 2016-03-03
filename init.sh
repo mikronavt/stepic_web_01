dce488facc4346c3d8be788b3e1e05fbbf0fdce6
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-03 10:47:04 +0300

    update starting gunicorn

--- init.sh
+++ init.sh
@@ -14,6 +14,6 @@ cd /
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
