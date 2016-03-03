b24387a6c362e3cfa686e01bb8ac04c31e972158
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-03 12:07:39 +0300

    update starting gunicorn

--- init.sh
+++ init.sh
@@ -16,3 +16,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+gunicorn -c /home/box/web/etc/gunicorn.conf 
