67d0d10f7a2f4a79e9c9d1131e30506c48aceab6
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-10 17:27:07 +0300

    tets

--- test.sh
+++ test.sh
@@ -5,7 +5,9 @@
 #curl -I http://10.42.19.225/js/test.js
 #echo "http://10.42.19.225/uploads/test.js"
 #curl -I http://10.42.19.225/uploads/test.js
-echo "http://0.0.0.0/hello/?a=1&a=2&b=3"
-curl "http://0.0.0.0/hello/?a=1&a=2&b=3"
-echo "http://0.0.0.0:8080/?a=1&a=2&b=3"
-curl "http://0.0.0.0:8080/?a=1&a=2&b=3"
+#echo "http://0.0.0.0/hello/?a=1&a=2&b=3"
+#curl "http://0.0.0.0/hello/?a=1&a=2&b=3"
+#echo "http://0.0.0.0:8080/?a=1&a=2&b=3"
+#curl "http://0.0.0.0:8080/?a=1&a=2&b=3"
+curl -vv http://0.0.0.0:8000/login/
+curl -vv http://0.0.0.0:8000/question/199/
