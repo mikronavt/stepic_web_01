b2e69f4b7bdab3d0cc5c540fe3195c15d5d092be
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-11 09:40:30 +0300

    nginx

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -10,13 +10,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', 'qa.views.test'),
+    url(r'', 'qa.views.test'),
     url(r'^login/.*$', 'qa.views.test'),
     url(r'^signup/.*$', 'qa.views.test'),
     url(r'^question/(\d+)/.*$', 'qa.views.test'),
     url(r'^ask/.*$', 'qa.views.test'),
     url(r'^popular/.*$', 'qa.views.test'),
     url(r'^new/.*$', 'qa.views.test'),
-    url(r'^./*$', 'qa.views.err'),
-
 )
