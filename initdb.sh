da99e3be95a1b8ded19ede8387f9e4de67752e5d
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-15 15:40:15 +0300

    db settings

--- initdb.sh
+++ initdb.sh
@@ -2,6 +2,4 @@ sudo /etc/init.d/mysql restart
 mysql -uroot -e "create database my_py_db;"
 mysql -uroot -e "create user web_py identified by 'test123';"
 mysql -uroot -e "grant all on my_py_db.* to web_py;"
-mysql --user web_py --password=test123 my_py_db
-show tables;
-exit;
+#mysql --user web_py --password=test123 my_py_db
