0b26cff2919ff083a4cf364636f1b995c696a8a2
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-15 15:15:06 +0300

    test

--- test.sh
+++ test.sh
@@ -9,13 +9,16 @@
 #curl "http://0.0.0.0/hello/?a=1&a=2&b=3"
 #echo "http://0.0.0.0:8080/?a=1&a=2&b=3"
 #curl "http://0.0.0.0:8080/?a=1&a=2&b=3"
-echo "login"
-curl -vv http://0.0.0.0:8000/login/
-echo "question"
-curl -vv http://0.0.0.0:8000/question/199/
-echo "ask/popular"
-curl -vv http://0.0.0.0:8000/ask//popular/
-echo "blabla"
-curl -vv http://0.0.0.0:8000/blablabla/
-echo "/"
-curl -vv http://0.0.0.0:8000/
+#echo "login"
+#curl -vv http://0.0.0.0:8000/login/
+#echo "question"
+#curl -vv http://0.0.0.0:8000/question/199/
+#echo "ask/popular"
+#curl -vv http://0.0.0.0:8000/ask//popular/
+#echo "blabla"
+#curl -vv http://0.0.0.0:8000/blablabla/
+#echo "/"
+#curl -vv http://0.0.0.0:8000/
+mysql -uroot -e "SELECT User FROM mysql.user;"
+mysql -uroot -e "show grants for web_py;"
+mysql -uroot -e  "show tables;"
