9d9063cf09c088a75ba5d6dcd3365dfa613dc477
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-15 12:37:32 +0300

    init mysql db

--- initdb.sh
+++ initdb.sh
@@ -1,4 +1,4 @@
-/etc/init.d/mysql start
+sudo /etc/init.d/mysql restart
 mysql -uroot -e "create database my_py_db"
 mysql -uroot -e "create user web_py identified by 'test123'"
 mysql -uroot -e "grant all on my_py_db.* to web_py"
