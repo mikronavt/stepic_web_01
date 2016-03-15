a15b8dedbd905ccb2de1a9d91936321623a79447
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-15 12:49:23 +0300

    init mysql db

--- initdb.sh
+++ initdb.sh
@@ -1,4 +1,7 @@
 sudo /etc/init.d/mysql restart
-mysql -uroot -e "create database my_py_db"
-mysql -uroot -e "create user web_py identified by 'test123'"
-mysql -uroot -e "grant all on my_py_db.* to web_py"
+mysql -uroot -e "create database my_py_db;"
+mysql -uroot -e "create user web_py identified by 'test123';"
+mysql -uroot -e "grant all on my_py_db.* to web_py;"
+mysql -uroot -e "SELECT User FROM mysql.user;"
+mysql -uroot -e "show grants for web_py;"
+mysql --user web_py --password=test123 my_py_db
