51832ad7ac3d1c6d8cdc938f342c69fefd210850
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-15 18:56:09 +0300

    questions_lists

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.http import HttpResponse
+from models import Question
 
 # Create your views here.
-from django.http import HttpResponse
+
 
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
+def questions_lists_all(request):
+    questions = Question.objects.filter()
+    questions = questions.order_by('-id')
+    limit = request.GET.get('limit', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(questions, limit)
+    paginator.baseurl = '/?page='
+    page = paginator.page(page)
+    return render(request, 'questions_list.html', {
+        'questions': page.object_list,
+        'paginator': paginator, 'page': page,
+    })
+
+def popular(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def question(request, slug):
+    return HttpResponse('OK')
