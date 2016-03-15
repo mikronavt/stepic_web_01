0c5664177fb635d52cd94cb38a286cd2bcdeda2e
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-15 20:37:16 +0300

    model

--- init.sh
+++ init.sh
@@ -13,10 +13,10 @@ mkdir js
 cd /
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-#sudo /etc/init.d/nginx restart
-sudo /etc/init.d/nginx stop
+sudo /etc/init.d/nginx restart
+#sudo /etc/init.d/nginx stop
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-#gunicorn -D -c /home/box/web/hello.py hello:app
+gunicorn -D -c /home/box/web/hello.py hello:app
 cd /home/box/web/ask
-gunicorn -D ask.wsgi:application --bind 0.0.0.0:80
+gunicorn -D ask.wsgi:application --bind 0.0.0.0:8000
