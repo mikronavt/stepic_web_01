d2cd4626c2312b887a0a09848165594b7cb6aaed
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-15 19:49:43 +0300

    model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
@@ -14,7 +14,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
     def __unicode__(self):
