04e91c099eaf70b0171d3dc85b66cb3e792be301
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-16 16:40:02 +0300

    forms

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
+
 )
 
 ROOT_URLCONF = 'ask.urls'
