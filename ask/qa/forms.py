7efd594dd670b83e4732cc5518cd8c28daa4f457
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-16 14:21:24 +0300

    forms

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -7,4 +7,4 @@ class AskForm(forms.Form):
 
 class AnswerForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
-    question = forms.CharField(widget=forms.HiddenInput)
+    question = forms.IntegerField(widget=forms.HiddenInput)
