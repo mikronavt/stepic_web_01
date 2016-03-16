bb6d0ed133811a55d7f2ace1095f1a85af5f679d
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-16 18:11:14 +0300

    forms

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -61,8 +61,7 @@ def ask(request):
         if form.is_valid():
             question = form.save()
             url = question.get_url()
-            return HttpResponse("OK")
-        #    return HttpResponseRedirect(url)
+            return HttpResponseRedirect(url)
     else:
         form = AskForm()
     return render(request, 'ask_form.html',{
