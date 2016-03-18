e5f3737121ecd399401023acec8eac8923b60f23
Tsaplin Anatoliy <chgb-tol@ya.ru>
2016-03-18 14:48:56 +0300

    login

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -64,7 +64,8 @@ def ask(request):
         if request.user.is_authenticated():
             form = AskForm(request.POST)
             form._user = request.user
-            return HttpResponse(form)
+            a = form.text.join(form.title).join(form._user)
+            return HttpResponse(a)
             if form.is_valid():
                 return HttpResponse('OK')
 
